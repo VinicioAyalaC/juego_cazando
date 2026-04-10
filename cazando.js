82f63a0e36fa34d4932f1ff90dc769214c314dbf
VinicioAyalaC <vhac2311@gmail.com>
2026-04-09 20:53:56 -0500

    finalizar parte 1

---
+++ cazando.js
@@ -0,0 +1,60 @@
+let canvas = document.getElementById("juego");
+let ctx = canvas.getContext("2d");
+
+
+// GATO
+let gatox=0;
+let gatoy=0;
+const ANCHOGATO=50;
+const ALTURAGATO=50;
+const VELOCIDAD=15;
+
+// COMIDA
+let comidax=50;
+let comiday=50;
+const ANCHOCOMIDA=30;
+const ALTURACOMIDA=30;
+
+//ctx.fillStyle = "#be2222";
+
+// FUNCION PRINCIPAL PARA GRAFICAR
+function graficarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,ancho,alto);
+}
+
+// FUNCION PARA GRAFICAR GATO
+function graficarGato(){
+    graficarRectangulo(gatox,gatoy,ANCHOGATO,ALTURAGATO,"#000000");
+}
+
+// FUNCION PARA GRAFICAR COMIDA
+function graficarComida(){
+    graficarRectangulo(comidax,comiday,ANCHOCOMIDA,ALTURACOMIDA,"#ff0000");
+}
+
+// FUNCION INICIAR JUEGO
+function iniciarJuego(){
+    gatox = (canvas.width / 2) - (ANCHOGATO / 2);     
+    gatoy = (canvas.height / 2) - (ALTURAGATO / 2);
+
+    graficarGato();
+    graficarComida();
+}
+
+// MOVIMIENTO DEL GATO
+function mover(direccion){
+    if (direccion === "arriba") gatoy -= VELOCIDAD;     
+    if (direccion === "abajo") gatoy += VELOCIDAD;     
+    if (direccion === "izquierda") gatox -= VELOCIDAD;     
+    if (direccion === "derecha") gatox += VELOCIDAD;
+    graficarGato();
+}
+
+document.getElementById("btnArriba").onclick = () => mover("arriba");
+document.getElementById("btnAbajo").onclick = () => mover("abajo");
+document.getElementById("btnIzquierda").onclick = () => mover("izquierda");
+document.getElementById("btnDerecha").onclick = () => mover("derecha");
+
+
+iniciarJuego();
